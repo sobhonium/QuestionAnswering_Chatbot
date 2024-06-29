fae370a0a8ce62bb1eb8913d6ff06024c71af17a
Sobhan <[200~sobangithub@gmail.com~>
2024-06-29 16:59:24 +1200

    edit and add cli

--- run_bot_cli.py
+++ run_bot_cli.py
@@ -2,6 +2,7 @@
 
 from transformers import pipeline
 
+
 question_answerer = pipeline("question-answering", model="model/final_model")
 
 
@@ -10,13 +11,16 @@ def run_qa_bot_cli():
         context = f.read()
     print("type `exit` to end the question answering and exit.")
     while (True):
-        input_text = input(">> ")
+        input_text = input("\033[92m User>> ")
+
         if input_text == "exit":
             break
         output_text = question_answerer(question=input_text, context=context)["answer"]
-        print(output_text)
-    
+        print("\033[94m AI>> " + output_text)
+        print("\n")
 
 
 if __name__ == "__main__":
     run_qa_bot_cli()
+    
+    
